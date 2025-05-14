f99ab6e7854a47778d801b03e4be1819e075fcb6
SpTn3132 <135387635+SpTn3132@users.noreply.github.com>
2025-05-14 09:54:29 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ document.getElementById("soyUnBoton").addEventListener("click", CambioColor);
 let color;
 function CambioColor()
 {
+  console.log(color);
   if(color==true){
       document.body.style.backgroundColor = "red";
       color = false;
